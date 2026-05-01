8c90ff48ecb44c097a0f1ebfa2a5cf699a6341f6
carine_dos-santos@estudante.sesisenai.org.br <carine_dos-santos@estudante.sesisenai.i>
2026-05-01 15:15:39 -0300

    add-foi colocado login para usuario ao entrar no site

---
+++ script.js
@@ -0,0 +1,37 @@
+const formulario = document.querySelector('#formulario');
+
+formulario.addEventListener('submit', function(evento){
+    evento.preventDefault();
+
+    let acessoLiberado = true;
+
+    let nome = document.getElementById("nome").value;
+    let senha = document.getElementById("senha").value;
+
+    if(nome.trim() === ""){
+        alert(" Digite seu nome ");
+        acessoLiberado = false;
+    }
+
+    if(senha.trim() === ""){
+        alert("Digite a senha para continuar a aventura");
+        acessoLiberado = false;
+    }
+
+    if(nome !== "dora"){
+        alert("Usuário não reconhecido!");
+        acessoLiberado = false;
+    }
+
+    if(senha !== "1234"){
+        alert("Senha incorreta!");
+        acessoLiberado = false;
+    }
+
+    if(acessoLiberado){
+        alert("Vamos explorar!");
+        window.location.href = "home.html";
+    } else {
+        alert("usuário ou senha incorretos");
+    }
+});
